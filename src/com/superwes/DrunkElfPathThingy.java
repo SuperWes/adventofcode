092561c8b22cd078298538c6940878f1fb05d205
dev1 <dev1@asynchrony.com>
2015-12-03 12:32:31 -0600

    All the rest

--- src/com/superwes/DrunkElfPathThingy.java
+++ src/com/superwes/DrunkElfPathThingy.java
@@ -1,7 +1,61 @@
 package com.superwes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DrunkElfPathThingy {
     public int countVisits(String arg) {
-        return 1;
+        int x = 0, y = 0;
+        Set<House> visitedHouses = new HashSet<>();
+        visitedHouses.add(new House(x, y));
+        for (char c : arg.toCharArray()) {
+            switch (c) {
+                case '>':
+                    x += 1;
+                    break;
+                case '<':
+                    x -= 1;
+                    break;
+                case '^':
+                    y += 1;
+                    break;
+                case 'v':
+                    y -= 1;
+                    break;
+            }
+            visitedHouses.add(new House(x, y));
+        }
+        return visitedHouses.size();
+    }
+
+    private class House {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            House house = (House) o;
+
+            if (x != house.x) return false;
+            return y == house.y;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
+
+        private final int x;
+        private final int y;
+
+        public House(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
     }
 }
